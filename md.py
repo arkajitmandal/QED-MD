d008670890a777c256b792f984e1b6673351b9d0
arkajitmandal <arkajitmandal@gmail.com>
2021-08-13 21:59:11 -0400

    using object to represent everything inside a

--- md.py
+++ md.py
@@ -1,32 +1,37 @@
 import numpy as np
 from numpy.random import normal as gran
 
-def vv(x, p, param):
-    f1 = param.force(x, param) 
-    m = param.m 
-    dt = param.dt
-    v  = p/m 
-    x += v * dt + 0.5 * (f1/m) * (dt ** 2)   
-    f2 = param.force(x, param) 
-    p += 0.5 * (f1 + f2) * dt 
-    return x, p, f2
+def vv(dat):
+    f1 = dat.f1
+    m = dat.m 
+    dt = dat.dt
+    v  = dat.p/m 
+    dat.x += v * dt + 0.5 * (f1/m) * (dt ** 2)   
+    f2 = dat.F(dat) 
+    dat.p += 0.5 * (f1 + f2) * dt 
+    dat.f1 = f2
+    return dat
 
-def vvl(x, p, param, f1):
-    ndof = param.ndof
-    β  = param.β
-    v = p/param.m
-    dt = param.dt
-    λ = param.λ #/ param.m
-    σ = (2.0 * λ/(β * param.m )) ** 0.5
+def vvl(dat):
+    ndof = dat.ndof
+    β  = dat.β
+    v = dat.p/dat.m
+    dt = dat.dt
+    λ = dat.λ #/ dat.m
+    σ = (2.0 * λ/(β * dat.m )) ** 0.5
     ξ = gran(0, 1, ndof)  #np.array([0.5 * gran() for i in range(len(x))])
     θ = gran(0, 1, ndof) #np.array([gran() * 0.28867513459  for i in range(len(x))])
     c = 0.28867513459
-    A = (0.5 * dt**2) * (f1/param.m - λ * v) + (σ * dt**(3.0/2.0)) * (0.5 * ξ + c * θ) 
+    A = (0.5 * dt**2) * (dat.f1/dat.m - λ * v) + (σ * dt**(3.0/2.0)) * (0.5 * ξ + c * θ) 
     #---- X update -----------
-    x += (v * dt + A) 
+    dat.x += (v * dt + A) 
     #-------------------------
-    f2 = param.force(x, param)
+    f1 = dat.f1
+    f2 = dat.F(dat)
     #---- V update ----------- 
-    v += ( 0.5 * dt * (f1+f2)/param.m - dt * λ * v +  σ * (dt**0.5) * ξ - A * λ ) 
+    v += ( 0.5 * dt * (f1+f2)/dat.m - dt * λ * v +  σ * (dt**0.5) * ξ - A * λ ) 
     #-------------------------
-    return x, v * param.m, f2
+    dat.f1 = f2
+    return dat
+
+
